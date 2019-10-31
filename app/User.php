ff1bb0cafe3df3dcca73ab2f6ea33e5a12b32b3e
Denis <denisborisenko982@mail.ru>
2019-10-31 15:44:14 +0300

    jwt installation and setup

--- app/User.php
+++ app/User.php
@@ -6,8 +6,9 @@ use App\Model\Question;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -40,5 +41,16 @@ class User extends Authenticatable
 
     public function question(){
         return $this->hasMany(Question::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
