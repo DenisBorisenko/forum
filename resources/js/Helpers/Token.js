2032e1c0f37a91390dcc1a240e2c6960874edc09
Denis <denisborisenko982@mail.ru>
2019-11-02 09:54:34 +0300

    helpers with login, localstorage

---
+++ resources/js/Helpers/Token.js
@@ -0,0 +1,17 @@
+class Token{
+    payload(token){
+        const payload =  token.split('.')[1]
+         return this.decode(payload)
+    }
+    decode(payload){
+        return JSON.parse(atob(payload))
+    }
+    isValid(token){
+        const payload = this.payload(token)
+        if( payload){
+            return payload.iss == 'http://127.0.0.1:8000/api/auth/login' ? true : false
+        }
+        return false
+    }
+}
+export default Token = new Token()
--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,8 @@ import User from './Helpers/user'
 window.User = User
 Vue.use(Vuetify)
 
+console.log(User.id())
+
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
